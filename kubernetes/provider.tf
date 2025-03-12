b1a21bd8b42f255f0eb49f9ac7d688668e64b608
abhishek pawar <ap053974@gmail.com>
2025-03-12 07:29:55 +0000

    new files

--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.24.0"
+    }
+  }
+}
 
+provider "google" {
+  credentials = file("final-project-453412-c40ff94ed0c2.json")
+  project     = var.project_id
+  region      = var.region
+}
